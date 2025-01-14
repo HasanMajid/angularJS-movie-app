f724157040afb26c253efb90940a0fd9fc52a534
Hasan Majid <hasanmajid3.14@hotmail.com>
2025-01-14 09:50:57 -0500

    retrieves movies on initial page load

--- app/app.js
+++ app/app.js
@@ -1,7 +1,11 @@
 var myNinjaApp = angular.module("MovieApp", []);
 
 myNinjaApp.controller("MovieSearchController", function ($scope, $http) {
+  console.log("Controller loaded");
   $scope.search = function (searchText) {
+    if (searchText === "") {
+      return;
+    }
     const url =
       "https://www.freetestapi.com/api/v1/movies?search=" + searchText;
 
@@ -16,6 +20,15 @@ myNinjaApp.controller("MovieSearchController", function ($scope, $http) {
     );
   };
   $scope.movies = [];
+  $http.get("https://www.freetestapi.com/api/v1/movies").then(
+    function (response) {
+      $scope.movies = response.data;
+    },
+    function (error) {
+      console.log("Error getting movies");
+      console.log(error);
+    }
+  );
 });
 // Before application runs
 myNinjaApp.config(function () {});
